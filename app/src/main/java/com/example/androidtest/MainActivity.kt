1d0ec34871aacce5986fefe99336cc3373b3b8f2
DrSmuc <107368724+DrSmuc@users.noreply.github.com>
2025-03-25 00:03:45 +0100

    final

--- app/src/main/java/com/example/androidtest/MainActivity.kt
+++ app/src/main/java/com/example/androidtest/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.androidtest
 
 import android.R.attr.value
+import android.content.Context
 import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
 import android.util.Log
+import android.view.ContextMenu
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -14,6 +18,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Locale
 
 
 class MainActivity : AppCompatActivity() {
@@ -25,6 +30,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.my_toolbar))
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -75,20 +81,15 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        val inflater = menuInflater
-        inflater.inflate(R.menu.menu_main, menu)
-        return super.onCreateOptionsMenu(menu)
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("COUNTER_VALUE", counter)
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.restore_counter -> {
-                // kod za 19. zadatak
-                true
-            }
-        }
-        return super.onOptionsItemSelected(item)
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        counter = savedInstanceState.getInt("COUNTER_VALUE", 0)
+        display.text = counter.toString()
     }
 
     fun refresh() {
@@ -126,5 +127,67 @@ class MainActivity : AppCompatActivity() {
         super.onRestart()
         Toast.makeText(applicationContext, "onRestart", Toast.LENGTH_SHORT).show()
         Log.i("MyLog", "onRestart")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    // Odabir opcije iz izbornika
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.restore_counter -> {
+                counter = 0
+                display.text = counter.toString()
+                return true
+            }
+
+            // Dodavanje opcija za promjenu jezika
+            R.id.croatian -> {
+                changeLanguage(this, "hr")  // Pozivanje funkcije za promjenu jezika na hrvatski
+                Toast.makeText(this, "Jezik promijenjen na Hrvatski", Toast.LENGTH_SHORT).show()
+                recreate()  // Ponovno učitaj aktivnost kako bi se primijenile promjene
+                return true
+            }
+            R.id.english -> {
+                changeLanguage(this, "en")  // Pozivanje funkcije za promjenu jezika na engleski
+                Toast.makeText(this, "Language changed to English", Toast.LENGTH_SHORT).show()
+                recreate()  // Ponovno učitaj aktivnost kako bi se primijenile promjene
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    // Funkcija za promjenu jezika
+    @Suppress("DEPRECATION")
+    fun changeLanguage(context: Context, language: String) {
+        val locale = Locale(language)
+        Locale.setDefault(locale)
+        val res = context.resources
+        val config = Configuration(res.configuration)
+        config.setLocale(locale)
+        context.createConfigurationContext(config)  // Ovo osigurava da se promjena jezika primijeni
+        res.updateConfiguration(config, res.displayMetrics)
+    }
+
+    // KONTEKSTUALNI IZBORNIK
+    override fun onCreateContextMenu(menu: ContextMenu, v: View, menuInfo: ContextMenu.ContextMenuInfo?) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        menuInflater.inflate(R.menu.menu_float, menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.reset_counter -> {
+                counter = 0
+                display.text = counter.toString()
+                Toast.makeText(this, "Brojač resetiran!", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onContextItemSelected(item)
+        }
     }
 }
--- app/src/main/java/com/example/androidtest/SuccessActivity.kt
+++ app/src/main/java/com/example/androidtest/SuccessActivity.kt
@@ -1,122 +1,72 @@
 package com.example.androidtest
 
-import android.app.PendingIntent
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.telephony.SmsManager
 import android.widget.Button
-import android.widget.Spinner
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class SuccessActivity : AppCompatActivity() {
-
-    private val spinner: Spinner? = null
-    private var counter = 0;
-    private var name = "";
+    lateinit var textView: TextView
+    lateinit var editTextMessage: EditText
+    lateinit var sendSMSButton: Button
+    lateinit var radioGroup: RadioGroup
+    lateinit var radioButtonPhone1: RadioButton
+    lateinit var radioButtonPhone2: RadioButton
+    lateinit var radioButtonPhone3: RadioButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_success)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
+        textView = findViewById(R.id.textView)
+        editTextMessage = findViewById(R.id.textView)
+        sendSMSButton = findViewById(R.id.buttonSendSMS)
+        radioGroup = findViewById(R.id.radioGroup)
+        radioButtonPhone1 = findViewById(R.id.radioButtonPhone1)
+        radioButtonPhone2 = findViewById(R.id.radioButtonPhone2)
+        radioButtonPhone3 = findViewById(R.id.radioButtonPhone3)
 
-        /*
-        val backbut = findViewById<Button>(R.id.backbut)
-        backbut.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+        val name = intent.getStringExtra("name")
+        textView.text = "$name, uspješno ste došli do 10 koraka."
 
-        }
-        */
-
-        var spinner: Spinner? = null
-        spinner = this.spinner
-
-
-        val sub1: Button = findViewById<Button>(R.id.sendSmsButton)
-        sub1.setOnClickListener {
-            val data: String = "Num: $counter, $name";
-            startActivity(getSendSmsIntent("0977510243", data))
-
+        sendSMSButton.setOnClickListener {
+            sendSMS()
         }
     }
 
-    override fun onStart() {
-        super.onStart();
-        val bundle = intent.extras;
-        val value = bundle!!.getInt("counter");
-        val value2 = bundle!!.getString("name");
-        counter = value;
-        if (value2 != null) {
-            name = value2
-        }
-
-        val sendButton: Button = findViewById(R.id.sendSmsButton);
-        /*sendButton.setOnClickListener() {
-            val num = "0977510243";
-            val msg = "Num: $counter, Name: $name";
-
-            val smsIntent = Intent(Intent.ACTION_SENDTO).apply {
-                data = Uri.parse("smto:$num");
-                putExtra("sms_body", msg);
+    private fun sendSMS() {
+        val selectedPhoneNumber = when (radioGroup.checkedRadioButtonId) {
+            R.id.radioButtonPhone1 -> radioButtonPhone1.text.toString()
+            R.id.radioButtonPhone2 -> radioButtonPhone2.text.toString()
+            R.id.radioButtonPhone3 -> radioButtonPhone3.text.toString()
+            else -> {
+                Toast.makeText(this, "Molimo odaberite broj telefona!", Toast.LENGTH_SHORT).show()
+                return
             }
-        }*/
+        }
 
-    }
+        val message = editTextMessage.text.toString()
 
-    /*
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        if (message.isEmpty()) {
+            Toast.makeText(this, "Unesite tekst poruke!", Toast.LENGTH_SHORT).show()
+            return
+        }
 
+        val intent = Intent(Intent.ACTION_VIEW).apply {
+            data = Uri.parse("sms:$selectedPhoneNumber")
+            putExtra("sms_body", message)
+        }
 
-        val backbut = findViewById<Button>(R.id.backbut)
-        backbut.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
+        try {
             startActivity(intent)
-
+        } catch (e: Exception) {
+            Toast.makeText(this, "Ne mogu pokrenuti SMS aplikaciju!", Toast.LENGTH_SHORT).show()
         }
-
-        var spinner: Spinner? = null
-        spinner = this.spinner
-
-
-        val sub1: Button = findViewById<Button>(R.id.sub1)
-        sub1.setOnClickListener {
-            val data: String = "Num: $counter, Name: $name";
-            startActivity(getSendSmsIntent("1234567", data))
-
-        }
-    }*/
-// textview_selected!!.text = "Selected : "+ Spinner [position]
-
-
-    private fun getSendSmsIntent(phoneNumber: String, content: String?): Intent? {
-
-        val uri = Uri.parse("smsto:$phoneNumber")
-        val intent = Intent(Intent.ACTION_SENDTO, uri)
-
-        intent.putExtra("sms_body", content)
-
-        return getIntent(intent, true)
-    }
-    private fun getIntent(intent: Intent, isNewTask: Boolean): Intent? {
-        return if (isNewTask) intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK) else intent
-
-    }
-
-    private fun sendSMS(phoneNumber: String, message: String) {
-        val sentPI: PendingIntent = PendingIntent.getBroadcast(this, 0, Intent("SMS_SENT"), 0)
-        SmsManager.getDefault().sendTextMessage(phoneNumber, null, message, sentPI, null)
     }
 }
